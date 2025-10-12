82925fad0e824f0a6828b8984b3cfc5961e0793b
Ayoub-Gourari <ae.gourari@gmail.com>
2025-10-12 12:21:22 +0200

    CNN_mnist implementation

--- byzfl/fed_framework/models.py
+++ byzfl/fed_framework/models.py
@@ -207,7 +207,7 @@ class MLP_mnist(nn.Module):
 
     Examples:
     ---------
-    >>> model = logreg_mnist()
+    >>> model = MLP_mnist()
     >>> x = torch.randn(16, 28*28)  # Batch of 16 MNIST images
     >>> output = model(x)
     >>> print(output.shape)
@@ -227,6 +227,40 @@ class MLP_mnist(nn.Module):
         x = self.hidden2(x)
         x = F.relu(x)
         x = self.classification_layer(x)
+        return x
+    
+class CNN_mnist(nn.Module):
+    """
+    Peng & al. CNN Model for MNIST.
+
+    Description:
+    ------------
+    A simple CNN model for the MNIST dataset.
+
+    Examples:
+    ---------
+    >>> model = CNN_mnist()
+    >>> x = torch.randn(16, 28*28)  # Batch of 16 MNIST images
+    >>> output = model(x)
+    >>> print(output.shape)
+    torch.Size([16, 10])
+    """
+    def __init__(self, num_classes=10):
+        """Initialize the model parameters."""
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.fc1 = nn.Linear(32 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        """Perform a forward pass through the model."""
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 32 * 8 * 8)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
     
 # ---------------------------------------------------------------------------- #
