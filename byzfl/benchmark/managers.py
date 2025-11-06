90301e72f66bca7f64d437809d9f42856eb77b12
Ayoub-Gourari <ae.gourari@gmail.com>
2025-11-06 21:17:10 +0100

    template of the functions reading the controle variables for the measures

--- byzfl/benchmark/managers.py
+++ byzfl/benchmark/managers.py
@@ -357,6 +357,12 @@ class ParamsManager(object):
         path = ["benchmark_config", "nb_steps"]
         read = self._read_object(path)
         return self._parameter_to_use(default, read)
+    
+    def get_make_feature_measures(self):
+        pass
+
+    def get_compute_gradient_variance(self):
+        pass
 
     # ----------------------------------------------------------------------
     #  Model
